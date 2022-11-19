19c24f93238650d397922ad231a1cd382010a79a
Simon Rene <mail@dogtaggames.de>
2022-11-19 17:12:43 +0100

    Add functionality

--- myManager.py
+++ myManager.py
@@ -1,5 +1,128 @@
-
+from sqlalchemy import create_engine
 
 class myManager:
     def manage(self):
         print("MANAGING...")
+
+    
+    def initDB(self):
+        self.engine = create_engine('postgresql://postgres@localhost/Training', pool_pre_ping=True)
+
+        #engine = create_engine("postgresql://postgres@localhost/BAdb", echo=True, future=True)
+
+        athleteTableFile = open("createTableAthletes.txt", mode='r', encoding='utf-8')
+        athleteTableText = athleteTableFile.read()
+        athleteTableFile.close()
+
+        courseTableFile = open("createTableCourses.txt", mode='r', encoding='utf-8')
+        courseTableText = courseTableFile.read()
+        courseTableFile.close()
+
+        completedTableFile = open("createTableCompleted.txt", mode='r', encoding='utf-8')
+        completedTableText = completedTableFile.read()
+        completedTableFile.close()
+
+
+        # "commit as you go"
+        with self.engine.connect() as conn:
+            #conn.execute(("CREATE TABLE Persons3 (PersonID int,LastName varchar(255));"))
+            #conn.execute((athleteTableText))
+            #conn.execute((courseTableText))
+            conn.execute((completedTableText))
+            #conn.commit()
+    
+    def addTestData(self):
+        self.engine = create_engine('postgresql://postgres@localhost/Training', pool_pre_ping=True)
+        coursesTestFile = open("addTestData02.txt", mode='r', encoding='utf-8')
+        coursesTestText = coursesTestFile.read()
+        coursesTestFile.close()
+
+        athletesTableFile = open("addTestData01.txt", mode='r', encoding='utf-8')
+        athletesTestText = athletesTableFile.read()
+        athletesTableFile.close()
+
+        completedTableFile = open("addTestData03.txt", mode='r', encoding='utf-8')
+        completedTestText = completedTableFile.read()
+        completedTableFile.close()
+
+
+        # "commit as you go"
+        with self.engine.connect() as conn:
+            #conn.execute(("CREATE TABLE Persons3 (PersonID int,LastName varchar(255));"))
+            conn.execute((athletesTestText))
+            conn.execute((coursesTestText))
+            conn.execute((completedTestText))
+
+    def getCourseNumbers(self):
+        self.engine = create_engine('postgresql://postgres@localhost/Training', pool_pre_ping=True)
+        courseNumbersSQL = "getCourseNumbers.txt"
+        courseNumbersSQLFile = open("getCourseNumbers.txt", mode='r', encoding='utf-8')
+        courseNumbersSQLText = courseNumbersSQLFile.read()
+        courseNumbersSQLFile.close()
+        courseNumbers = []
+        with self.engine.connect() as conn:
+            #conn.execute(("CREATE TABLE Persons3 (PersonID int,LastName varchar(255));"))
+            data1 = conn.execute((courseNumbersSQLText))
+            numbers = data1.fetchall()
+            for n in numbers:
+                courseNumbers.append(n[0])
+        return courseNumbers
+    
+    def getAthleteNumbers(self):
+        self.engine = create_engine('postgresql://postgres@localhost/Training', pool_pre_ping=True)
+        courseNumbersSQLFile = open("getAthleteNumbers.txt", mode='r', encoding='utf-8')
+        courseNumbersSQLText = courseNumbersSQLFile.read()
+        courseNumbersSQLFile.close()
+        courseNumbers = []
+        with self.engine.connect() as conn:
+            #conn.execute(("CREATE TABLE Persons3 (PersonID int,LastName varchar(255));"))
+            data1 = conn.execute((courseNumbersSQLText))
+            numbers = data1.fetchall()
+            for n in numbers:
+                courseNumbers.append(n[0])
+        return courseNumbers
+    
+    def addAthlete(self, Name, Weight, Size, Gender):
+        self.engine = create_engine('postgresql://postgres@localhost/Training', pool_pre_ping=True)
+        addAthleteFile = open("addAthlete.txt", mode='r', encoding='utf-8')
+        addAthleteText = addAthleteFile.read()
+        addAthleteFile.close()
+        addAthleteFormated = addAthleteText.format(Name,Weight,Size,Gender)
+
+        with self.engine.connect() as conn:
+            #conn.execute(("CREATE TABLE Persons3 (PersonID int,LastName varchar(255));"))
+            conn.execute((addAthleteFormated))
+    
+    def addCourse(self, Designation):
+        if (not hasattr(self,'engine')):
+            self.engine = create_engine('postgresql://postgres@localhost/Training', pool_pre_ping=True)
+        addCourseFile = open("addCourse.txt", mode='r', encoding='utf-8')
+        addCourseText = addCourseFile.read()
+        addCourseFile.close()
+        addCourseFormated = addCourseText.format(Designation)
+
+        with self.engine.connect() as conn:
+            #conn.execute(("CREATE TABLE Persons3 (PersonID int,LastName varchar(255));"))
+            conn.execute((addCourseFormated))
+    
+    def deleteCourse(self, TNr):
+        if (not hasattr(self,'engine')):
+            self.engine = create_engine('postgresql://postgres@localhost/Training', pool_pre_ping=True)
+        deleteCourseFile = open("deleteCourse.txt", mode='r', encoding='utf-8')
+        deleteCourseText = deleteCourseFile.read()
+        deleteCourseFile.close()
+        deleteCourseFormated = deleteCourseText.format(TNr)
+
+        with self.engine.connect() as conn:
+            conn.execute((deleteCourseFormated))
+
+    def deleteAthlete(self, ID):
+        if (not hasattr(self,'engine')):
+            self.engine = create_engine('postgresql://postgres@localhost/Training', pool_pre_ping=True)
+        deleteAthleteFile = open("deleteAthlete.txt", mode='r', encoding='utf-8')
+        deleteAthleteText = deleteAthleteFile.read()
+        deleteAthleteFile.close()
+        deleteAthleteFormated = deleteAthleteText.format(ID)
+
+        with self.engine.connect() as conn:
+            conn.execute((deleteAthleteFormated))
--- windowSwitch.py
+++ windowSwitch.py
@@ -3,11 +3,20 @@ import tkinter as tk                # python 3
 from tkinter import font as tkfont  # python 3
 from tkinter import Grid
 
+import myManager
 
 class SampleApp(tk.Tk):
 
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
+
+        self.man = myManager.myManager()
+
+        #self.man.initDB()
+        #self.man.addTestData()
+        #self.man.getCourseNumbers()
+
+        self.man.getCourseNumbers()
 
         self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
 
@@ -84,22 +93,22 @@ class PageOne(tk.Frame):
         entry02.grid(row=1,column=1)
 
         # ID selector
-        variableID = tk.StringVar(gridFrame)
-        variableID.set("001") # default value
-        idList = ["001", "002"]
-        entryID = tk.OptionMenu(gridFrame, variableID, *idList)
+        self.variableID = tk.StringVar(gridFrame)
+        athleteNumbers = controller.man.getAthleteNumbers()
+        self.variableID.set(athleteNumbers[0]) # default value
+        self.entryID = tk.OptionMenu(gridFrame, self.variableID, *athleteNumbers)
         label03 = tk.Label(gridFrame, text="ID", font=controller.title_font)
         label03.grid(row=0,column=2)
-        entryID.grid(row=0,column=3)
+        self.entryID.grid(row=0,column=3)
 
         # Button - Create Athlete
         button = tk.Button(gridFrame, text="Create Athlete",
-                           command=lambda: controller.show_frame("StartPage"))
+                           command=lambda: (controller.man.addAthlete(entry02.get(),75,180,variableGender.get()), self.refresh()))
         button.grid(row=3,column=1)
 
         # Button - Delete Athlete
         button02 = tk.Button(gridFrame, text="Delete Athlete",
-                           command=lambda: controller.show_frame("StartPage"))
+                           command=lambda: (controller.man.deleteAthlete(self.variableID.get()), self.refresh()))
         button02.grid(row=1,column=3)
         
         # Button - Back to Main Menue
@@ -109,6 +118,17 @@ class PageOne(tk.Frame):
 
         gridFrame.pack(padx=20,pady=20)
 
+    def refresh(self):
+        athleteNumbers = self.controller.man.getAthleteNumbers()
+        #print(athleteNumbers)
+        self.variableID.set(athleteNumbers[0]) # default value
+        #self.entryID.configure(courseNumbers)
+        menu = self.entryID["menu"]
+        menu.delete(0, "end")
+        for string in athleteNumbers:
+            menu.add_command(label=string, 
+                             command=lambda value=string: self.variableID.set(value))
+
 
 class PageTwo(tk.Frame):
 
@@ -117,9 +137,57 @@ class PageTwo(tk.Frame):
         self.controller = controller
         label = tk.Label(self, text="Create Course", font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
-        button = tk.Button(self, text="Go to the start page",
-                           command=lambda: controller.show_frame("StartPage"))
-        button.pack()
+        
+        gridFrame = tk.Frame(self)
+        
+        # Designation Field
+        label01 = tk.Label(gridFrame, text="Designation", font=controller.title_font)
+        label01.grid(row=0,column=0)
+        entry01 = tk.Entry(gridFrame)
+        entry01.grid(row=0,column=1,ipadx=30)
+
+        # Name Field
+        label02 = tk.Label(gridFrame, text="Description", font=controller.title_font)
+        label02.grid(row=1,column=0)
+        entry02 = tk.Entry(gridFrame)
+        entry02.grid(row=1,column=1)
+
+        # ID selector
+        self.variableID = tk.StringVar(gridFrame)
+        courseNumbers = controller.man.getCourseNumbers()
+        self.variableID.set(courseNumbers[0]) # default value
+        self.entryID = tk.OptionMenu(gridFrame, self.variableID, *courseNumbers)
+        label03 = tk.Label(gridFrame, text="TNr", font=controller.title_font)
+        label03.grid(row=0,column=2)
+        self.entryID.grid(row=0,column=3)
+
+        # Button - Create Athlete
+        button = tk.Button(gridFrame, text="Create Course",
+                           command=lambda:( controller.man.addCourse(entry01.get()), self.refresh() ))
+        button.grid(row=3,column=1)
+
+        # Button - Delete Athlete
+        button02 = tk.Button(gridFrame, text="Delete Course",
+                           command=lambda: (controller.man.deleteCourse(self.variableID.get()), self.refresh() ))
+        button02.grid(row=1,column=3)
+        
+        # Button - Back to Main Menue
+        backButton = tk.Button(gridFrame, text="Back",
+                           command=lambda: (controller.show_frame("StartPage")))
+        backButton.grid(row=5,column=0)
+
+        gridFrame.pack(padx=20,pady=20)
+    
+    def refresh(self):
+        courseNumbers = self.controller.man.getCourseNumbers()
+        #print(courseNumbers)
+        self.variableID.set(courseNumbers[0]) # default value
+        #self.entryID.configure(courseNumbers)
+        menu = self.entryID["menu"]
+        menu.delete(0, "end")
+        for string in courseNumbers:
+            menu.add_command(label=string, 
+                             command=lambda value=string: self.variableID.set(value))
 
 class PageThree(tk.Frame):
 
